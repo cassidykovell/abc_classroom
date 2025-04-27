dafbc0b63a2f9ba3fccc4a98ca38e8861ecb369a
cassidy <cassidykovell@Mac.lan>
2025-04-27 16:50:35 -0700

    correct database info and connect correct

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,21 @@
-const mongoose = require('mongoose');
-require ("dotenv").config()
-console.log(process.env.MONGODB_URI)
+const mongoose = require("mongoose")
+require("dotenv").config()
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// Set strictQuery to suppress the deprecation warning
+mongoose.set("strictQuery", false)
 
+console.log("Attempting to connect to MongoDB...")
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connection successful!"))
+  .catch((err) => {
+    console.error("MongoDB connection error:", err)
+    console.log("Please check your connection string and make sure your IP address is whitelisted in MongoDB Atlas.")
+  })
 
 module.exports = mongoose.connection
